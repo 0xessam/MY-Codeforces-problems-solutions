0ad44029f29c666cc8a72aebd4c349c33f786c8c
essam <essamhishamdev@gmail>
2023-01-01 18:25:00 +0300

    Add solution for problem `B - Coins` Link: https://codeforces.com/contest/47/problem/B Tags: implementation, *1200 Ref: https://codeforces.com/contest/47/submission/187536690

---
+++ codeforces/47/B.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std ;
+#define ll long long
+#define ull unsigned long long
+#define fixed(n) fixed << setprecision(n)
+#define sz(x) int(x.size())
+#define xessamx cin.sync_with_stdio(0); cin.tie(0);
+#define all(x) x.begin(), (x).end()
+#define sortv(v) sort(all(v))
+#define ppp push_back
+#define TxtIO   freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
+#define el '\n'
+#define yes cout << "YES" << el
+#define no cout << "NO" << el
+#define FOR(i, n) for(int i = 0; i < (n); ++i)
+#define FORR(i, m , n) for(int i = n; i >=m ; --i)
+#define FORV(i, n,x,vvv) for(int i = 0; i < (n); ++i) { int x ; cin >> x ; vvv.ppp(x);}
+typedef pair<ll, ll> pll;
+typedef vector<ll> vll;
+typedef vector<pll> vpll;
+typedef vector<string> vs;
+typedef map<ll,ll> mll;
+#define ff first
+#define ss second
+#define pb push_back
+#define mp make_pair
+bool Prime(ll n){if(n<=1)return false;if(n<=3)return true;if(n%2==0||n%3==0)return false;for(int i=5;i*i<=n;i=i+6)if(n%i==0||n%(i+2)==0)return false;return true;}
+bool isPowerOfTwo(int n){if(n==0)return false;return (ceil(log2(n)) == floor(log2(n)));}
+bool isPerfectSquare(ll x){if (x >= 0) {ll sr = sqrt(x);return (sr * sr == x);}return false;}
+void solllve()
+{
+	string s ;
+	ll A{},B{},C{} ;
+	FOR(i,3)
+	{
+		cin>>s ; 
+		if (s[1]=='>')
+		{
+			if (s[0]=='A')A++ ;
+			else if (s[0]=='B')B++;
+			else if (s[0]=='C')C++ ;
+		}
+		else
+		{
+			if (s[02]=='A')A++ ;
+			else if (s[02]=='B')B++;
+			else if (s[02]=='C')C++ ;
+		}
+		
+	}
+	
+	int arr[3]{0} ; 
+	arr[0]=A ; arr[1]=B ; arr[2]=C ;
+	sort(arr,arr+3) ;
+	if (arr[0]==0&&arr[1]==1&&arr[2]==2)
+	{
+		string t ("") ;
+		if (A==2) t+= 'A' ;
+		else if (B==2) t+= 'B' ;
+		else if (C==2) t+= 'C' ;
+		
+		if (A==1)  t+= 'A' ;
+		else if (B==1) t+= 'B' ;
+		else if (C==1) t+= 'C' ;
+		
+		if (!A) t+= 'A' ;
+		else if (!B) t+= 'B' ;
+		else if (!C) t+= 'C' ;
+		reverse(all(t)) ; cout << t ;
+	}
+	else cout << "Impossible" ;
+	//cout << A << ' ' << B << ' ' << C ;
+ 
+}
+int main()
+{
+	//TxtIO
+	xessamx
+	int t = 1 ;
+	// cin >> t ;
+	while (t--)
+	{
+		solllve() ;
+	}
+ 
+	return 0;
+}
