46c61f1d80239b31092eac2d37ddb8c55c4e7253
essam <essamhishamdev@gmail>
2023-01-16 13:37:00 +0300

    Add solution for problem `A - Wizards' Duel` Link: https://codeforces.com/contest/591/problem/A Tags: implementation, math, *900 Ref: https://codeforces.com/contest/591/submission/189422155

---
+++ codeforces/591/A.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std ;
+    
+int main (){
+
+    double a,b,c ; cin >> a >> b >> c ;
+    cout << fixed << setprecision(12)<<a/ (1+(c/b)) ; 
+    
+return 0 ;
+}
