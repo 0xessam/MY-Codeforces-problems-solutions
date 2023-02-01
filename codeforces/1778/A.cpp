635e830ab6b92a4e69f4d4657bec5898d950e116
essam <essamhishamdev@gmail>
2023-02-02 01:03:00 +0300

    Add solution for problem `A - Flip Flop Sum` Link: https://codeforces.com/contest/1778/problem/A Tags: greedy, implementation, *800 Ref: https://codeforces.com/contest/1778/submission/191634112

---
+++ codeforces/1778/A.cpp
@@ -0,0 +1,60 @@
+// اللهم صل و سلم و بارك على سيدنا محمد
+#include <bits/stdc++.h>
+using namespace std ;
+#define ll long long
+#define ull unsigned long long
+#define fixed(_) fixed << setprecision(_)
+#define sz(x) int(x.size())
+#define all(x) x.begin(), (x).end()
+#define sortv(v) sort(all(v))
+#define TxtIO   freopen("math.in","r",stdin); //freopen("output.txt","w",stdout);
+#define el '\n'
+#define yes cout << "YES" << el
+#define no cout << "NO" << el
+#define FOR(i, n) for(int i = 0; i < (n); ++i)
+#define oo LONG_LONG_MAX
+const int mod=1e9+7;
+const int N=1e5+5;
+
+
+
+void solllve()
+{
+
+    int n ,sm=0,cn=0;
+    cin >> n ;
+    int a[n];
+    for (int i=0 ; i<n ; i++ )
+    {
+        cin >> a[i];
+        cn+=(a[i]==-1);
+        sm+=a[i];
+    }
+    if (cn>1)
+    {
+        for (int i=0 ; i<n ; i++ )
+        {
+            if (a[i]==-1 and a[i+1]==-1) {sm+=4,i++ ;break  ;}
+        }
+    }
+    else if (cn==0)
+        sm-=4;
+    cout <<sm<<endl ;
+
+
+
+
+}
+int main()
+{
+    //TxtIO
+    cin.sync_with_stdio(0); cin.tie(0);
+    int t = 1 ;
+    cin >> t ;
+    while (t--)
+    {
+        solllve() ;
+    }
+
+    return 0;
+}
