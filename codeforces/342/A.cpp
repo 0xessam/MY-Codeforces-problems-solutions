fa3d4a98a9b007bf09328c9ce38ac6a46a4db561
essam <essamhishamdev@gmail>
2023-03-22 08:46:00 +0300

    Add solution for problem `A - Xenia and Divisors` Link: https://codeforces.com/contest/342/problem/A Tags: greedy, implementation, *1200 Ref: https://codeforces.com/contest/342/submission/198580626

--- codeforces/342/A.cpp
+++ codeforces/342/A.cpp
@@ -1,6 +1,6 @@
- /* Only a fool learns from his own mistakes. The wise man learns from the mistakes of others.*/
+    /* Only a fool learns from his own mistakes. The wise man learns from the mistakes of others.*/
     /*{ وَالَّذِينَ جَاهَدُوا فِينَا لَنَهْدِيَنَّهُمْ سُبُلَنَا ۚ وَإِنَّ اللَّهَ لَمَعَ الْمُحْسِنِينَ }*/
- 
+
     #include <bits/stdc++.h>
     using namespace std ;
     #define ll long long
@@ -27,12 +27,12 @@
     #define show(x)  cout << #x << " = " << x << '\n'
     #define cvc(x) for (auto &i : x) cin >> i ;
     #define cm return void
- 
+
     void solllve () ;
- 
+
     int main()
     {
- 
+
         // TxtIO
         cin.sync_with_stdio(0); cin.tie(0);
         int t = 1 ;
@@ -43,7 +43,7 @@
         }
         return 0;
     }
- 
+
   void solllve()
     {
        int n ; cin >> n ;
@@ -63,5 +63,5 @@
             FOR (i,m[4]) cout <<"1 2 4\n" ;
         }
        FOR (i,m[6]-m[3]) cout << "1 2 6\n" ;
- 
+
     }
