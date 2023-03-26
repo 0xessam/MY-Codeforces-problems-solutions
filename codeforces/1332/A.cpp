3f9a14899659469c9be64919bf1240cf620e3b28
essam <essamhishamdev@gmail>
2023-03-26 03:34:00 +0300

    Add solution for problem `A - Exercising Walk` Link: https://codeforces.com/contest/1332/problem/A Tags: greedy, implementation, math, *1100 Ref: https://codeforces.com/contest/1332/submission/199165937

---
+++ codeforces/1332/A.cpp
@@ -0,0 +1,73 @@
+    /* Only a fool learns from his own mistakes. The wise man learns from the mistakes of others.*/
+    /*{ وَالَّذِينَ جَاهَدُوا فِينَا لَنَهْدِيَنَّهُمْ سُبُلَنَا ۚ وَإِنَّ اللَّهَ لَمَعَ الْمُحْسِنِينَ }*/
+
+    #include <bits/stdc++.h>
+    using namespace std ;
+    #define ll long long
+    #define ull unsigned long long
+    #define fixed(_) fixed << setprecision(_)
+    #define sz(x) int(x.size())
+    #define all(x) x.begin(), (x).end()
+    #define rall(x) x.rbegin(), (x).rend()
+    #define TxtIO  freopen("hobz.in","r",stdin);// freopen("output.txt","w",stdout);
+    #define el '\n'
+    #define ff first
+    #define ss second
+    #define yes cout << "YES" << el
+    #define no cout << "NO" << el
+    #define maxz(x, y) x = max(x, y)
+    #define minz(x, y) x = min(x, y)
+    #define FOR(i, n) for(int i = 0 ; i < (n); ++i)
+    #define oo LONG_LONG_MAX
+    #define io INT_MAX
+    #define srt(x) sort(x.begin(),x.end())
+    #define rsrt(x) sort(x.rbegin(),x.rend())
+    const int mod=1e9+7;
+    const int N=2e5+5;
+    #define show(x)  cout << #x << " = " << x << '\n'
+    #define cvc(x) for (auto &i : x) cin >> i ;
+    #define cm return void
+
+    void solllve () ;
+
+    int main()
+    {
+
+        // TxtIO
+        cin.sync_with_stdio(0); cin.tie(0);
+        int t = 1 ;
+        cin >> t ;
+        while (t--)
+        {
+
+            solllve() ;
+        }
+        return 0;
+    }
+
+    void solllve()
+    {
+     int a,b,c,d ; cin >> a >> b >> c >> d ;
+     int x,y,x1,y1,x2,y2 ; cin >> x >> y >> x1 >> y1 >> x2 >> y2 ;
+     int d1 = abs (a-b) ;
+     if (a>b)
+     {
+         if (abs(x1-x)<d1 or ((a or b) and (!abs(x1-x) and !abs(x2-x)) ) ) cm(no) ;
+     }
+     else
+     {
+         if (abs(x2-x)<d1 or ((a or b) and (!abs(x1-x) and !abs(x2-x)) ) ) cm(no) ;
+     }
+     d1 = abs (c-d) ;
+
+        if (c>d)
+        {
+            if (abs(y1-y)<d1 or ((c or d) and (!abs(y1-y) and !abs(y2-y)) ) ) cm(no) ;
+        }
+        else
+        {
+            if (abs(y2-y)<d1 or ((c or d) and (!abs(y1-y) and !abs(y2-y)) )  ) cm(no) ;
+        }
+
+        yes ;
+    }
