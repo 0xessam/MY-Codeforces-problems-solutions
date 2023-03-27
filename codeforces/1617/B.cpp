d1529ae8ba607174666b3d64aee40a1c8d54df74
essam <essamhishamdev@gmail>
2023-03-27 07:18:00 +0300

    Add solution for problem `B - GCD Problem` Link: https://codeforces.com/contest/1617/problem/B Tags: brute force, constructive algorithms, math, number theory, *900 Ref: https://codeforces.com/contest/1617/submission/199358730

---
+++ codeforces/1617/B.cpp
@@ -0,0 +1,88 @@
+/* Only a fool learns from his own mistakes. The wise man learns from the mistakes of others.*/
+/*{ وَالَّذِينَ جَاهَدُوا فِينَا لَنَهْدِيَنَّهُمْ سُبُلَنَا ۚ وَإِنَّ اللَّهَ لَمَعَ الْمُحْسِنِينَ }*/
+
+#include <bits/stdc++.h>
+using namespace std ;
+#define ll long long
+#define ull unsigned long long
+#define fixed(_) fixed << setprecision(_)
+#define sz(x) int(x.size())
+#define all(x) x.begin(), (x).end()
+#define rall(x) x.rbegin(), (x).rend()
+#define TxtIO  freopen("hobz.in","r",stdin);// freopen("output.txt","w",stdout);
+#define el '\n'
+#define ff first
+#define ss second
+#define yes cout << "YES" << el
+#define no cout << "NO" << el
+#define maxz(x, y) x = max(x, y)
+#define minz(x, y) x = min(x, y)
+#define FOR(i, n) for(int i = 0 ; i < (n); ++i)
+#define oo LONG_LONG_MAX
+#define io INT_MAX
+#define srt(x) sort(x.begin(),x.end())
+#define rsrt(x) sort(x.rbegin(),x.rend())
+const int mod=1e9+7;
+const int N=2e5+5;
+#define show(x)  cout << #x << " = " << x << '\n'
+#define cvc(x) for (auto &i : x) cin >> i ;
+#define cm return void
+
+void solllve () ;
+
+int main()
+{
+
+    // TxtIO
+    cin.sync_with_stdio(0); cin.tie(0);
+    int t = 1 ;
+    cin >> t ;
+    while (t--)
+    {
+        solllve() ;
+    }
+    return 0;
+}
+
+
+bool prime(int n)
+{
+    // Corner cases
+    if (n <= 1)
+        return false;
+    if (n <= 3)
+        return true;
+
+    // This is checked so that we can skip
+    // middle five numbers in below loop
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+
+    // Using concept of prime number
+    // can be represented in form of
+    // (6*n + 1) or(6*n - 1) hence
+    // we have to go for every multiple of 6 and
+    // prime number would always be 1 less or 1 more than
+    // the multiple of 6.
+    for (int i = 5; i * i <= n; i = i + 6)
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+
+    return true;
+}
+void solllve()
+{
+    int n ; cin >> n ;
+    for (int i=2 ; ; i++)
+    {
+        if (prime(i) and (n-i-1)%i )
+        {
+            cout<< n-i-1  << ' ' << i << ' ' << 1 << el ;
+            break;
+        }
+    }
+
+
+
+    //ااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااه
+}
