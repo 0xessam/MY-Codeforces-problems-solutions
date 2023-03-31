05af15e651b1610fcf3aa7203e42dc2effb90e29
essam <essamhishamdev@gmail>
2023-04-01 02:10:00 +0300

    Add solution for problem `C - Dominated Subarray` Link: https://codeforces.com/contest/1257/problem/C Tags: greedy, implementation, sortings, strings, two pointers, *1200 Ref: https://codeforces.com/contest/1257/submission/200056061

---
+++ codeforces/1257/C.cpp
@@ -0,0 +1,92 @@
+/* Only a fool learns from his own mistakes. The wise man learns from the mistakes of others.*/
+/*{ وَالَّذِينَ جَاهَدُوا فِينَا لَنَهْدِيَنَّهُمْ سُبُلَنَا ۚ وَإِنَّ اللَّهَ لَمَعَ الْمُحْسِنِينَ }*/
+
+#include <bits/stdc++.h>
+using namespace std ;
+#define ll long long
+#define ull unsigned long long
+#define fixed(_) fixed << setprecision(_)
+#define sz(x) int(x.size())
+#define all(x) x.begin(), (x).end()
+#define rall(x) x.rbegin(), (x).rend()
+#define TxtIO  freopen("hobz.in","r",stdin);// freopen("output.txt","w",stdout);
+#define el '\n'
+#define ff first
+#define ss second
+#define yes cout << "Yes" << el
+#define no cout << "No" << el
+#define maxz(x, y) x = max(x, y)
+#define minz(x, y) x = min(x, y)
+#define FOR(i, n) for(int i = 0 ; i < (n); ++i)
+#define oo LONG_LONG_MAX
+#define io INT_MAX
+#define srt(x) sort(x.begin(),x.end())
+#define rsrt(x) sort(x.rbegin(),x.rend())
+const int mod=1e9+7;
+const int N=2e5+5;
+#define show(x)  cout << #x << " = " << x << '\n'
+#define cvc(x) for (auto &i : x) cin >> i ;
+#define cm return void
+
+void solllve () ;
+
+
+int main()
+{
+
+    // TxtIO
+    cin.sync_with_stdio(0); cin.tie(0);
+    int t = 1 ;
+    cin >> t ;
+    while (t--)
+    {
+        solllve() ;
+    }
+    return 0;
+}
+int n ;
+vector<int>v ;
+bool val = 0 ;
+bool ok (int ml)
+{
+    if (ml>n)ml=n ; 
+    map<int,int>m ;
+    for (int i=0 ; i<ml ; ++i)
+    {
+        m[v[i]]++ ;
+        if (m[v[i]]==2) { val=1 ; return 1; }
+    }
+    int l=0 ;
+    for (int i=ml ; i<n ; ++i)
+    {
+        m[v[l]]-- ; l++ ;
+        m[v[i]]++ ;
+        if (m[v[i]]==2) {
+            val = 1;
+            return 1;
+        }
+    }
+    return 0 ;
+}
+void solllve()
+{
+    val=0 ;
+    cin >> n ;
+    v = vector<int>(n) ;
+    cvc(v) ;
+    ll l=1 , r=2*n-1 ;
+    while (r>l+1)
+    {
+        ll m = l + (r-l)/2 ;
+        if (ok(m)) r=m ;
+        else l=m ;
+    }
+    if (val)
+        cout << r << el ;
+    else cout << -1 << el ;
+
+
+
+
+    //ااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااه
+}
