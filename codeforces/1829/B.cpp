5175150b87984ca167167f8e4fa890efd539e36e
essam <essamhishamdev@gmail>
2023-05-06 17:41:00 +0300

    Add solution for problem `B - Blank Space` Link: https://codeforces.com/contest/1829/problem/B Tags: implementation, *800 Ref: https://codeforces.com/contest/1829/submission/204767507

--- codeforces/1829/B.cpp
+++ codeforces/1829/B.cpp
@@ -1,44 +1,78 @@
-#include <iostream>
-
-using namespace std;
+// https://www.youtube.com/watch?v=zzyDvsnXGg4
+/* لم يَكُنْ رسولُ اللَّهِ صلَّى اللَّهُ عليهِ وسلَّمَ يَدَعُ هؤلاءِ الكلمات
+  حينَ يُمسي وحينَ يصبحُ اللَّهمَّ إنِّي أسألُكَ العفو والعافيةَ في الدُّنيا والآخرةِ
+  اللَّهمَّ إنِّي أسألُكَ العفوَ والعافيةَ في ديني ودُنْيايَ وأَهْلي ومالي اللَّهمَّ استُر عَوْراتي وآمِن رَوعاتي
+  اللَّهمَّ احفَظني من بينِ يديَّ ومن خَلفي وعن يميني وعن شِمالي ومِن فَوقي وأعوذ بعَظمتِكَ أن أُغتالَ مِن تَحتي
+  */
+// There is no need for sadness and despair
+#include <bits/stdc++.h>
+using namespace std ;
+#define ll long long
+#define ull unsigned long long
+#define fixed(_) fixed << setprecision(_)
+#define sz(x) int(x.size())
+#define all(x) x.begin(), (x).end()
+#define rall(x) x.rbegin(), (x).rend()
+#define TxtIO  freopen("hobz.in","r",stdin);// freopen("output.txt","w",stdout);
+#define el '\n'
+#define ff first
+#define ss second
+#define yes cout << "YES" << el
+#define yess cm (cout << "YES" << el)
+#define no cout << "NO" << el
+#define noo cm(cout << "NO" << el)
+#define none cm (cout << "-1" << el)
+#define maxz(x, y) x = max(x, y)
+#define minz(x, y) x = min(x, y)
+#define FOR(i, n) for(int i = 0 ; i < (n); ++i)
+#define oo LONG_LONG_MAX
+#define io INT_MAX
+#define srt(x) sort(x.begin(),x.end())
+#define rsrt(x) sort(x.rbegin(),x.rend())
+const int mod=1e9+7;
+const int N=2e5+5;
+#define show(x)  cout << #x << " = " << x << '\n'
+#define cvc(x) for (auto &i : x) cin >> i ;
+#define cm return void
+# define mod(a,b) ((a - 1) % b + b) % b;
+#define vct(n) vector<int>v(n) ; \
+ cvc(v) ;
+// sort(a+1,a+n+1,[](long long x,long long y){return abs(x)>abs(y);});
+void solllve () ;
 
 int main()
 {
-    int t;
-    cin >> t;
-    while(t--)
+
+    // TxtIO
+    cin.sync_with_stdio(0); cin.tie(0);
+    int t = 1 ;
+    cin >> t ;
+
+    while (t--)
     {
-        int n;
-        cin >> n;
-        int bin[n];
-
-        int  c = 0 , maxx = 0 , input;
-        for(int i=0; i<n; i++)
-        {
-            cin >> input;
-
-            if (input == 0)
-            {
-                bin[i] = input;
-                c++;
-                if ((i == n-1) && (c > maxx) )
-                {
-                    maxx = c;
-                    break;
-                }
-            }
-            else
-            {
-                if (c > maxx)
-                {
-                    maxx = c;
-                  
-                }
-c=0; 
-            }
-
-        }
-if ( c>maxx) maxx= c ; 
-        cout << maxx << endl;
+        solllve() ;
     }
+    return 0;
 }
+
+
+
+void solllve()
+{
+    int n ; cin >> n ;
+    vector<int>v(n) ;
+    cvc(v) ;
+    int c = 1 ;
+    int res= 0 ;
+    for (int i=0 ; i<n-1 ; i++)
+    {
+        if (v[i]==v[i+1] and v[i]==0) c++ ;
+        else maxz(res,c),c=1 ;
+    }
+    maxz(res,c) ;
+    if (std::count(v.begin(), v.end(),0)==0) cm(cout << 0 << el) ;
+    cout << res << el ;
+
+}
+
+
