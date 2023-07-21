7de0ec25fc451ac4d2d4e03bb1302b4ef7e99153
essam <essamhishamdev@gmail>
2023-07-21 21:35:00 +0300

    Add solution for problem `E - Cardboard for Pictures` Link: https://codeforces.com/contest/1850/problem/E Tags: binary search, geometry, implementation, math, *1100 Ref: https://codeforces.com/contest/1850/submission/214949649

---
+++ codeforces/1850/E.cpp
@@ -0,0 +1,100 @@
+// https://www.youtube.com/watch?v=zzyDvsnXGg4
+/* لم يَكُنْ رسولُ اللَّهِ صلَّى اللَّهُ عليهِ وسلَّمَ يَدَعُ هؤلاءِ الكلمات
+  حينَ يُمسي وحينَ يصبحُ اللَّهمَّ إنِّي أسألُكَ العفو والعافيةَ في الدُّنيا والآخرةِ
+  اللَّهمَّ إنِّي أسألُكَ العفوَ والعافيةَ في ديني ودُنْيايَ وأَهْلي ومالي اللَّهمَّ استُر عَوْراتي وآمِن رَوعاتي
+  اللَّهمَّ احفَظني من بينِ يديَّ ومن خَلفي وعن يميني وعن شِمالي ومِن فَوقي وأعوذ بعَظمتِكَ أن أُغتالَ مِن تَحتي
+  */
+// There is no need for sadness and despair
+#include <bits/stdc++.h>
+using namespace std ;
+#define ll long long
+#define ull unsigned long long
+#define fixed(_) fixed << setprecision(_)
+#define sz(x) int(x.size())
+#define all(x) x.begin(), (x).end()
+#define rall(x) x.rbegin(), (x).rend()
+#define TxtIO  freopen("hobz.in","r",stdin);// freopen("output.txt","w",stdout);
+#define el '\n'
+#define ff first
+#define ss second
+#define yes cout << "YES" << el
+#define yess cm (cout << "YES" << el)
+#define no cout << "NO" << el
+#define noo cm(cout << "NO" << el)
+#define none cm (cout << "-1" << el)
+#define maxz(x, y) x = max(x, y)
+#define minz(x, y) x = min(x, y)
+#define FOR(i, n) for(int i = 0 ; i < (n); ++i)
+#define oo LONG_LONG_MAX
+#define io INT_MAX
+#define srt(x) sort(x.begin(),x.end())
+#define rsrt(x) sort(x.rbegin(),x.rend())
+const int mod=1e9+7;
+const int N=2e5+5;
+#define show(x)  cout << #x << " = " << x << '\n'
+#define cvc(x) for (auto &i : x) cin >> i ;
+#define cm return void
+# define mod(a,b) ((a - 1) % b + b) % b;
+#define vct(n) vector<int>v(n) ; \
+ cvc(v) ;
+const int dc[8] = {0, 0, 1, -1, 1, 1, -1, -1};
+const int dr[8] = {1, -1, 0, 0, 1, -1, 1, -1};
+// sort(a+1,a+n+1,[](long long x,long long y){return abs(x)>abs(y);});
+void solllve () ;
+
+int main()
+{
+
+    // TxtIO
+    cin.sync_with_stdio(0); cin.tie(0);
+    int t = 1 ;
+    cin >> t ;
+
+    while (t--)
+    {
+        solllve() ;
+    }
+    return 0;
+}
+
+ll x=0 , n , a ;
+
+bool val(ll m )
+{
+    return 4ll*x+ 4ll*m*n > a/m ;
+}
+double vl(ll m )
+{
+    return 4ll*x+ 4ll*m*n - a/m ;
+}
+
+void solllve()
+{
+    cin >> n ;
+    x=0 ;
+    cin >> a ;
+    FOR(i,n)
+    {
+        ll z ; cin >> z ;
+        x+=z ;
+        a-=1ll*z*z ;
+    }
+    ll l=1 , r = 1e18/n , m ;
+    while (l<r+1)
+    {
+        m = (r+l)/2 ;
+        if (val(m)) r=m-1 ;
+        else l=m+1 ;
+    }
+    // if (vl(r+1) < vl(r))
+    {
+        //      cout << r+1 << el ;
+    }
+    // else
+    cout << r << el ;
+
+
+
+
+
+}
