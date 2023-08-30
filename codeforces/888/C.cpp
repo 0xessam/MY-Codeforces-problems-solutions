351a9eb57aa1a9e0142354a5c6a3eb03baf61f2a
essam <essamhishamdev@gmail>
2023-08-30 23:55:00 +0300

    Add solution for problem `C - K-Dominant Character` Link: https://codeforces.com/contest/888/problem/C Tags: binary search, implementation, two pointers, *1400 Ref: https://codeforces.com/contest/888/submission/221197043

---
+++ codeforces/888/C.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std ;
+#define lp(i,n) for(int i = 0; i < (n); ++i)
+template<typename T>istream& operator>>(istream&i,vector<T>&v){lp(j,v.size())i>>v[j];return i;}
+#define tenll(x) ((ll)1e##x)
+#define ll long long
+#define el '\n'
+#define int ll
+#define ull unsigned long long
+#define cm return void
+#define yes cout << "Yes" << el
+#define yess cm (cout << "Yes" << el)
+#define no cout << "No" << el
+#define noo cm(cout << "No" << el)
+#define none cm (cout << "-1" << el)
+#define maxz(x, y) x = max(x, y)
+#define minz(x, y) x = min(x, y)
+#define show(x)  cout << #x << " = " << x << '\n'
+#define setDP(arr) memset(arr,-1,sizeof arr)
+#define cout(v) for(auto i : v)cout << i << ' ' ; \
+cout << el ;
+void __(){
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout);
+#endif
+}
+
+
+void solllve () ;
+signed main()
+{
+
+    __() ;
+    int t = 1 ;
+    //  cin >> t ;
+    while (t--)
+    {
+        solllve() ;
+    }
+    return 0;
+}
+
+void solllve()  {
+    string s ; cin >> s ;
+    vector<vector<int>> idx(('z')+5) ;
+    for (int i = 'a'; i <='z'; ++i) {
+        idx[i].push_back(-1) ;
+    }
+    for (int i = 0; i < s.size(); ++i) {
+        idx[s[i]].push_back(i) ;
+    }
+    for (int i = 'a'; i <='z'; ++i) {
+        idx[i].push_back(s.size()) ;
+    }
+    int mn = 1e9 , mx = -1;
+    for (int i = 'a'; i <='z'; ++i) {
+        mx=-1 ;
+        for (int j = 0; j+1 < idx[i].size() ; ++j) {
+            maxz(mx, idx[i][j+1]-idx[i][j] ) ;
+        }
+       if(~mx) minz(mn,mx) ;
+    }
+
+    if (mn==1e9)cout <<  (ll)(s.size()+2)/2 ;
+     else cout << mn << el ;
+
+
+
+
+
+}
+
+/* لو أنكم تتوكلون على الله حق توكله لرزقكم كما يرزق الطير تغدو خماصاً وتروح بطاناً */
