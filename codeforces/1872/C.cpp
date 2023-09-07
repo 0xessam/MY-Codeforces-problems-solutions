ec8af3a8a898463e4000d239e0f5ac6e8bc67957
essam <essamhishamdev@gmail>
2023-09-07 18:50:00 +0300

    Add solution for problem `C - Non-coprime Split` Link: https://codeforces.com/contest/1872/problem/C Tags: math, number theory, *1100 Ref: https://codeforces.com/contest/1872/submission/222299781

--- codeforces/1872/C.cpp
+++ codeforces/1872/C.cpp
@@ -25,92 +25,59 @@ void __(){
      freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout);
 #endif
  }
+# define MAXN 10000007
+vector<int>spf(10000007) ;
 
-vector<int> sieve(const int N, const int Q = 17, const int L = 1 << 15) {
-  static const int rs[] = {1, 7, 11, 13, 17, 19, 23, 29};
-  struct P {
-    P(int p) : p(p) {}
-    int p; int pos[8];
-  };
-  auto approx_prime_count = [] (const int N) -> int {
-    return N > 60184 ? N / (log(N) - 1.1)
-                     : max(1., N / (log(N) - 1.11)) + 1;
-  };
+// Calculating SPF (Smallest Prime Factor) for every
+// number till MAXN.
+// Time Complexity : O(nloglogn)
+void sieve()
+{
+    spf[1] = 1;
+    for (int i = 2; i < MAXN; i++)
 
-  const int v = sqrt(N), vv = sqrt(v);
-  vector<bool> isp(v + 1, true);
-  for (int i = 2; i <= vv; ++i) if (isp[i]) {
-    for (int j = i * i; j <= v; j += i) isp[j] = false;
-  }
+        // marking smallest prime factor for every
+        // number to be itself.
+        spf[i] = i;
 
-  const int rsize = approx_prime_count(N + 30);
-  vector<int> primes = {2, 3, 5}; int psize = 3;
-  primes.resize(rsize);
+    // separately marking spf for every even
+    // number as 2
+    for (int i = 4; i < MAXN; i += 2)
+        spf[i] = 2;
 
-  vector<P> sprimes; size_t pbeg = 0;
-  int prod = 1;
-  for (int p = 7; p <= v; ++p) {
-    if (!isp[p]) continue;
-    if (p <= Q) prod *= p, ++pbeg, primes[psize++] = p;
-    auto pp = P(p);
-    for (int t = 0; t < 8; ++t) {
-      int j = (p <= Q) ? p : p * p;
-      while (j % 30 != rs[t]) j += p << 1;
-      pp.pos[t] = j / 30;
+    for (int i = 3; i * i < MAXN; i++) {
+        // checking if i is prime
+        if (spf[i] == i) {
+            // marking SPF for all numbers divisible by i
+            for (int j = i * i; j < MAXN; j += i)
+
+                // marking spf[j] if it is not
+                // previously marked
+                if (spf[j] == j)
+                    spf[j] = i;
+        }
     }
-    sprimes.push_back(pp);
-  }
-
-  vector<unsigned char> pre(prod, 0xFF);
-  for (size_t pi = 0; pi < pbeg; ++pi) {
-    auto pp = sprimes[pi]; const int p = pp.p;
-    for (int t = 0; t < 8; ++t) {
-      const unsigned char m = ~(1 << t);
-      for (int i = pp.pos[t]; i < prod; i += p) pre[i] &= m;
-    }
-  }
-
-  const int block_size = (L + prod - 1) / prod * prod;
-  vector<unsigned char> block(block_size); unsigned char* pblock = block.data();
-  const int M = (N + 29) / 30;
-
-  for (int beg = 0; beg < M; beg += block_size, pblock -= block_size) {
-    int end = min(M, beg + block_size);
-    for (int i = beg; i < end; i += prod) {
-      copy(pre.begin(), pre.end(), pblock + i);
-    }
-    if (beg == 0) pblock[0] &= 0xFE;
-    for (size_t pi = pbeg; pi < sprimes.size(); ++pi) {
-      auto& pp = sprimes[pi];
-      const int p = pp.p;
-      for (int t = 0; t < 8; ++t) {
-        int i = pp.pos[t]; const unsigned char m = ~(1 << t);
-        for (; i < end; i += p) pblock[i] &= m;
-        pp.pos[t] = i;
-      }
-    }
-    for (int i = beg; i < end; ++i) {
-      for (int m = pblock[i]; m > 0; m &= m - 1) {
-        primes[psize++] = i * 30 + rs[__builtin_ctz(m)];
-      }
-    }
-  }
-  assert(psize <= rsize);
-  while (psize > 0 && primes[psize - 1] > N) --psize;
-  primes.resize(psize);
-  return primes;
 }
-void solllve () ;
 
-vector<int>v ;
+vector<int> getFactorization(int x)
+{
+    vector<int> ret;
+    while (x != 1) {
+        ret.push_back(spf[x]);
+        x = x / spf[x];
+    }
+    return ret;
+}
+
+
+
+void solllve () ;
 signed main()
 {
-
+     sieve() ;
      __() ;
      int t = 1 ;
      cin >> t ;
-     auto x = sieve(3500) ;
-     v=x ;
      while (t--)
      {
            solllve() ;
@@ -119,30 +86,19 @@ signed main()
 }
 
 
+void solllve()  {
+     int a,b ; cin >> a >> b;
+     if ( b-a>0 and b>3) cout << b-(b&1)-2 << ' ' << 2 << el ;
+     else if (b>3)
+     {
+         auto x = getFactorization(a) ;
+         if (x.size()==1) none ;
+         cout << a/x.front() << ' ' << a-a/x.front() << el ;
+     }
+     else none ;
 
-void solllve() {
 
-//        cout << ( 9834495/ 6274 ) ;
-//    int a ,b ; cin >> a >> b ;
-//    if (b%3==0 and b>3 ) cm(cout << b/3 << ' ' << 2*b/3 << el ) ;
-//    b-=b&1 ;
-//    if ( b >2 and b>=a ) cout << b-2 << ' ' << 2 << el ;
-//    else none ;
-    int a , b ; cin >> a >> b ;
-    if (b-a>=1 and b>3) cout << b-(b&1)-2 << ' ' << 2 << el ;
-    else if (a==b)
-    {
-        for (int i = 0; i < v.size(); ++i) {
-            if (a%v[i]==0 and a/v[i]>1)
-            {
-                cout << a/v[i] << ' ' << a-a/v[i] << el ;
-                return;
-            }
-        }
-        none ;
 
-    }
-    else none ;
 
 
 }
