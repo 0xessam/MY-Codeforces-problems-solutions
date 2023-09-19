794e120c24518df7260146018dc14818feb86e7f
essam <essamhishamdev@gmail>
2023-09-19 07:16:00 +0300

    Add solution for problem `D - Black and White Stripe` Link: https://codeforces.com/contest/1690/problem/D Tags: implementation, two pointers, *1000 Ref: https://codeforces.com/contest/1690/submission/223975241

---
+++ codeforces/1690/D.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std ;
+#define lp(i,n) for(int i = 0; i < (n); ++i)
+template<typename T>istream& operator>>(istream&i,vector<T>&v){lp(j,v.size())i>>v[j];return i;}
+#define tenll(x) ((ll)1e##x)
+#define ll long long
+#define el '\n'
+#define int ll
+#define ull unsigned long long
+#define cm return void
+#define yes cout << "Yes" << el
+#define yess cm (cout << "Yes" << el)
+#define no cout << "No" << el
+#define noo cm(cout << "No" << el)
+#define none cm (cout << "-1" << el)
+#define maxz(x, y) x = max(x, y)
+#define minz(x, y) x = min(x, y)
+#define show(x)  cout << #x << " = " << x << '\n'
+#define setDP(arr) memset(arr,-1,sizeof arr)
+#define cout(v) for(auto i : v)cout << i << ' ' ; \
+cout << el ;
+void __(){
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#ifndef ONLINE_JUDGE
+     freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout);
+#endif
+ }
+
+
+void solllve () ;
+signed main()
+{
+
+     __() ;
+     int t = 1 ;
+     cin >> t ;
+     while (t--)
+     {
+           solllve() ;
+     }
+       return 0;
+}
+
+
+void solllve()  {
+//    vector<int>idx;
+    int n,k ; cin >> n >> k ;
+    string s ; cin >> s ;
+    s.insert(s.begin() , '0' ) ;
+    int pre[200005]{};
+    for (int i = 0; i <= n; ++i) {
+        pre[i]+=s[i]=='B' ;
+    }
+    for (int i = 1; i <= n; ++i) {
+        pre[i]+=pre[i-1] ;
+    }
+    int l = 0 , r = k-1 ;
+    int mn = 1e9 ;
+    while (r<=n)
+    {
+        minz(mn , k-(pre[r]-pre[l-1])  ) ;
+        l++,r++ ;
+    }
+    cout << mn << el ;
+
+
+
+
+
+}
+
+/* لو أنكم تتوكلون على الله حق توكله لرزقكم كما يرزق الطير تغدو خماصاً وتروح بطاناً */
